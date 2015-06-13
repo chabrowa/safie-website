0441a0ca26065dbc3b6e2b751b158f75bc85f189
Mathieu Dutour <mathieu@dutour.me>
2015-06-13 14:47:40 +0100

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp  = require("gulp");
+var notify = require("gulp-notify");
+var less = require('gulp-less');
+var concat = require('gulp-concat');
+var autoprefixer = require('gulp-autoprefixer');
+
+var ENV = process.env.NODE_ENV || 'development';
+console.log('Running with NODE_ENV = ', ENV);
+
+// default error handler for stage/prod (exit with status 1)
+var errorHandler = function(err) {
+  console.log(err.message);
+  console.log('FAILED - exit(1)');
+  process.exit(1);
+};
+
+// use gulp-notify in development
+if (ENV === 'development') {
+  errorHandler = notify.onError(function(e) { return e.message; });
+}
+
+gulp.task('less', function() {
+  return gulp.src('app/styles/*/*.less')
+    .on('error', errorHandler)
+    .pipe(less())
+    .pipe(concat('client.css'))
+    .pipe(autoprefixer({
+      browsers: ['last 5 iOS versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('app/public/stylesheets'));
+});
+
+gulp.task('default', ['less'], function() {
+  gulp.watch(['app/styles/*/*.less'], ['less']);
+});
